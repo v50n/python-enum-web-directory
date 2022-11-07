e21782bd80697f50bd546fce40da11d7d0909abb
vson75 <vson.ng91@gmail.com>
2022-11-07 14:57:11 +0100

    Create enumdir.py

---
+++ enumdir.py
@@ -0,0 +1,20 @@
+import http.client
+
+print("---this program will try to get a specific directory in any webserver---")
+host = input("Insert here the host/IP: ")
+port = int(input("the port: "))
+dir = input("What directory you want to verify ? : ")
+
+if port == "":
+    port = 80
+
+try: 
+    connection = http.client.HTTPConnection(host,port)
+    connection.request('GET','/%s' % dir)
+    if connection.status = 200:
+        print("the web have the directory: /" dir)
+    else:
+        print("Not found :/",dir)
+    connection.close()
+except ConnectionRefusedError:
+    print("connection failed")
